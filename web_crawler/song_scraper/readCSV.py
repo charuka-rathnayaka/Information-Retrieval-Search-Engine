f58f2069e14e922bc22baa6096edc7fed1287b7f
Charuka <charuka.18@cse.mrt.ac.lk>
2023-01-06 18:53:55 +0530

    Adding corpus with the data clean function

---
+++ web_crawler/song_scraper/readCSV.py
@@ -0,0 +1,50 @@
+import csv
+import glob
+
+files = glob.glob("./*.csv")
+line_count = 0
+data_array = []
+for filename in files:
+    print(filename)
+    with open(filename,encoding='utf-8', errors='ignore') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        for row in csv_reader:
+            if (row[0] != 'name'):
+                name_singer = row[0].split("-")
+                song_name = name_singer[0].strip()
+                singer_name = name_singer[1].strip()
+                lyrics = row[1].strip()
+                u_album =row[2].replace('"', '')
+                u_year = row[3].replace('"', '')
+                if(len(u_album)>4):
+                    album = u_album.strip()
+                else:
+                    album='unknown'
+                if (len(u_year) > 2):
+                    year = u_year.strip()
+                else:
+                    year = 'unknown'
+
+                featured_artists = row[4].split(",")
+                featured_artists = [i.strip() for i in featured_artists]
+                metaphor_meaning = row[5].split('-')
+
+                metaphor = metaphor_meaning[0].replace('"', '').strip()
+                # print(metaphor)
+                meaning = metaphor_meaning[1].replace('"', '').strip()
+                data_array.append([song_name,singer_name,album,year,featured_artists,lyrics,metaphor,meaning])
+                line_count+=1
+# for a in data_array:
+#     print(a[0],a[1])
+#     # print(a[5])
+print("number of songs",line_count)
+
+fieldNames = ['Song Name', 'Singer', 'Album', 'Year', 'Featured Artists', 'Lyrics', 'Metaphor', 'interpretation']
+
+with open('text_corpus.csv', 'w',newline='',encoding='utf-8', errors='ignore') as f:
+    # using csv.writer method from CSV package
+    write = csv.writer(f)
+    write.writerow(fieldNames)
+    write.writerows(data_array)
+
+print("Data Saved  ---->  <----")
--- web_crawler/song_scraper/spiders/lyrics_spider.py
+++ web_crawler/song_scraper/spiders/lyrics_spider.py
@@ -1,10 +1,17 @@
 import scrapy
+import re
+CLEANR = re.compile('<.*?>|&([a-z0-9]+|#[0-9]{1,6}|#x[0-9a-f]{1,6});')
 
 
 class LyricsSpiderSpider(scrapy.Spider):
     name = 'lyrics_spider'
     allowed_domains = ['lyricslk.com']
-    start_urls = ['https://lyricslk.com/lyrics/artist/17-kasun-kalhara.html']
+    # start_urls = ['https://lyricslk.com/lyrics/artist/17-kasun-kalhara.html']
+    start_urls = ['https://lyricslk.com/search.php?q=2&by=forArtist&page=3']
+
+    def cleanhtml(self,raw_html):
+        cleantext = re.sub(CLEANR, '', raw_html)
+        return cleantext
 
     def parse(self, response):
         songurls = response.css('div.ResTitleSin a::attr("href")')
@@ -14,8 +21,11 @@ class LyricsSpiderSpider(scrapy.Spider):
     def parse_songs(self,response):
         songs = response.css('#lyricsViewer')
         for song in songs:
+            lyrics = song.css('#lyricsBody').get()
+            cleanedLyrics = self.cleanhtml(lyrics)
+
             yield {
                 'name': song.css('#lyricsTitle h2::text').get(),
-                'lyrics': song.css('#lyricsBody::text').get()
+                'lyrics':cleanedLyrics
             }
 
