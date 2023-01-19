283b36c04c68b941084d628c378eecfa77caa61f
Charuka <charuka.18@cse.mrt.ac.lk>
2023-01-19 18:01:45 +0530

    conversion function added

--- web_crawler/song_scraper/json_converter.py
+++ web_crawler/song_scraper/json_converter.py
@@ -3,12 +3,12 @@ import json
 from datetime import datetime
 
 # Opening JSON file
-json_file = open('./textCorpus.json',encoding="utf-8")
+json_file = open('./json_edited.json',encoding="utf-8")
 
 # returns JSON object as
 # a dictionary
 data = json.load(json_file)
-with open("songs_JSON.json", "w",encoding="utf-8") as outfile:
+with open("final_songs_JSON.json", "w",encoding="utf-8") as outfile:
     for json_ in data['songs']:
         json.dump(json_, outfile,ensure_ascii=False)
         outfile.write('\n')
