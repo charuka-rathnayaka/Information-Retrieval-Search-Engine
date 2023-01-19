13ed6571c388f66f543428fdb692f378b8dfb13c
Charuka <charuka.18@cse.mrt.ac.lk>
2023-01-19 17:59:38 +0530

    UI changes and features added

--- search-engine-ui/sinhala-song-search-engine/src/leftPanel.jsx
+++ search-engine-ui/sinhala-song-search-engine/src/leftPanel.jsx
@@ -16,26 +16,26 @@ const LeftPanel = (prop)=>{
                 sx={{}}
                 >
                 <Grid item sx={{ marginLeft:'20px', marginRight:'20px'}}>
-                    <Grid sx={{justifyContent:'center', marginLeft:'15%',marginBottom:'20px', fontSize:"20px", fontWeight:"500", paddingTop:'20px'}}>Filter by</Grid>
+                    <Grid sx={{justifyContent:'center', marginLeft:'10%',marginBottom:'20px', fontSize:"20px", fontWeight:"500", paddingTop:'20px', color:'black'}}>Filter by</Grid>
                     <div>
                         {prop.filters.map(function(filter, i){
                             return <div>
-                                    <Accordion sx={{marginTop:"10px"}}>
+                                    <Accordion sx={{marginTop:"10px", backgroundColor:'#9e9e9e', color:'black'}}>
                                         <AccordionSummary
                                         expandIcon={<ExpandMoreIcon />}
                                         aria-controls="panel1a-content"
                                         id="panel1a-header"
                                         >
-                                            <Grid item sx={{fontSize:'17px', justifyContent:'left', marginLeft:"20%"}}>
+                                            <Grid item sx={{fontSize:'17px', justifyContent:'left', marginLeft:"12%"}}>
                                                 {[Object.keys(filter)[0]]}
                                             </Grid>
                                         </AccordionSummary>
                                         <AccordionDetails>
-                                        <Grid item sx={{marginLeft:"25%"}}>
+                                        <Grid item sx={{marginLeft:"15%"}}>
                                     {filter[Object.keys(filter)[0]].map(function(value,j){
                                         return <div>
                                             <Grid container direction="row" alignItems='center'>
-                                                <Grid item xs={2}>
+                                                <Grid item xs={3}>
                                                     <Checkbox checked={prop.checkedFilters[i][Object.keys(filter)[0]][j]} onChange={()=>changeCheckedVal(i,Object.keys(filter)[0],j)}/>
                                                 </Grid>
                                                 <Grid item xs={9}>
